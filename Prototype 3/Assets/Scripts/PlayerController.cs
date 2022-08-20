866108c6b614c38a592f287ab6521cc92187c48f
isaccaldas <34041465+isaccaldas@users.noreply.github.com>
2022-08-20 14:08:34 -0300

    playerScript config

--- Prototype 3/Assets/Scripts/PlayerController.cs
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    private Rigidbody playerRb;
+    public float jumpForce = 10;
+    public float gravityModifier;
+    public bool isOnGround = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Atribui o componente Rigidbody do player a variável playerRB
+        playerRb = GetComponent<Rigidbody>();
+        //playerRb.AddForce(Vector3.up * 500);
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround){
+
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
         
+    }
+
+    public void OnCollisionEnter(Collision other) {
+             isOnGround = true;
     }
 }
